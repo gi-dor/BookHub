f1b0ebdc88149ab732413e9e7de9641102b03283
imkkkyeong <dlwldmsgo808@gmail.com>
2024-05-14 17:13:25 +0900

    Fix: search 오류 수정 Feat : 실시간 검색어 순위 (미완성 -Redis)

--- src/main/java/com/example/bookhub/main/controller/SearchController.java
+++ src/main/java/com/example/bookhub/main/controller/SearchController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SearchController {
     private final SearchService searchService;
 
-
     //  출시일 순 검색 했을때
     @GetMapping("/search")
     public String searchBooks(SearchCriteria searchCriteria, Model model) {;
@@ -28,3 +27,20 @@ public class SearchController {
 }
 
 
+    /*
+    // 검색 페이지로 이동 및 검색 수행
+    @GetMapping("/search")
+    public String searchBooks(SearchCriteria searchCriteria, Model model) {
+        // 검색어가 있을 경우에만 실시간 검색어 랭킹 업데이트
+        if (searchCriteria.getKeyword() != null && !searchCriteria.getKeyword().isEmpty()) {
+            searchService.updateSearchRanking();
+        }
+
+        // 검색 수행
+        BookListDto dto = searchService.keywordSearch(searchCriteria);
+        model.addAttribute("book", dto.getBooks()); // 모델에 책 정보를 담아서 HTML로 전달.
+        model.addAttribute("criteria", dto.getCriteria());
+        return "main/searchList"; // searchList.html로 반환
+    }
+
+*/
