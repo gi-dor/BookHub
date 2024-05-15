9491b94fd1287815e8170025c0bb4a232040de52
Minju Ji <hinote444@gmail.com>
2024-05-15 16:22:55 +0900

    Feat: 카테고리, 출판사, 저자 조회에 캐싱 처리

--- src/main/java/com/example/bookhub/common/CacheType.java
+++ src/main/java/com/example/bookhub/common/CacheType.java
@@ -8,18 +8,21 @@ import lombok.RequiredArgsConstructor;
 public enum CacheType {
 
     BOOK_GETAUTHORBYBOOKNO("BookMapper.getAuthorByBookNo", 10, 10000),
-    INQUIRIES("MyPageMapper.cacheInquiries" , 10,10000),
+    INQUIRIES("MyPageMapper.cacheInquiries", 10, 10000),
     USER_COUNT("dashBoardMapper.getAllUserCnt", 60, 10000),
     BOOK_COUNT("dashBoardMapper.getAllBookCnt", 60, 10000),
     TOTAL_DATE("dashBoardMapper.getTotalDate", 60, 10000),
-    NOANSWER_CNT("dashBoardMapper.noAnswerCnt", 60, 10000 ),
-    ANSWER_CNT("dashBoardMapper.answerCnt", 60, 10000 ),
-    GET_REVIEWS("dashBoardMapper.getReviews", 60, 10000 ),
-    AVERAGE_RATE("dashBoardMapper.averageRate", 60, 10000 ),
-    NOANSWER_RATIO("dashBoardMapper.noAnswerRatio", 60, 10000 ),
-    ANSWER_RATIO("dashBoardMapper.answerRatio", 60, 10000 ),
-    GET_RATIO("dashBoardMapper.getRatios", 60, 10000 );
-
+    NOANSWER_CNT("dashBoardMapper.noAnswerCnt", 60, 10000),
+    ANSWER_CNT("dashBoardMapper.answerCnt", 60, 10000),
+    GET_REVIEWS("dashBoardMapper.getReviews", 60, 10000),
+    AVERAGE_RATE("dashBoardMapper.averageRate", 60, 10000),
+    NOANSWER_RATIO("dashBoardMapper.noAnswerRatio", 60, 10000),
+    ANSWER_RATIO("dashBoardMapper.answerRatio", 60, 10000),
+    GET_RATIO("dashBoardMapper.getRatios", 60, 10000),
+    TOP_CATEGORY("topCategory", 3600, 20),
+    SECOND_CATEGORY("secondCategory", 3600, 1000),
+    PUBLISHER("publisher", 3600, 10000),
+    AUTHOR("author", 3600, 10000);
 
     private final String cacheName;
     private final int expiredAfterWrite;
